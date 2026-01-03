cba302cb1fcc516ee45f134d48aba45be26fab7e
dingzhan.sui <wujiu.sdz@outlook.com>
2026-01-03 20:09:00 +0800

    chore: init

---
+++ src/webview/src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" rel="noopener" target="_blank">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
